7fb6e45d7c7d0820c49af5ba73447912b0c85d5d
Nguyen258-fb <dtc245030038@ictu.edu.vn>
2026-09-21 21:46:29 +0700

    Add files via upload

---
+++ create_table_student..sql
@@ -0,0 +1,12 @@
+CREATE DATABASE my_database1;
+CREATE DATABASE my_database1;
+DROP DATABASE my_database1;
+CREATE DATABASE demo;
+USE demo;
+
+CREATE TABLE Student (
+    id INT,
+    name VARCHAR(200),
+    age INT,
+    country VARCHAR(50)
+);
